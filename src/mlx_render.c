162e2df8228609d611df99c814eabb41a03b3802
Karim <k.mehour@gmail.com>
2023-06-10 23:03:43 -0400

    Add pallette in renderer

--- src/mlx_render.c
+++ src/mlx_render.c
@@ -11,8 +11,19 @@ double mod2(double v[2])
 {
 	return (v[0] * v[0] + v[1] * v[1]);
 }
+int norm_rgba(float r, float g, float b, float a)
+{
+	r = fmin(fmax(r, 0), 1) * 255;
+	g = fmin(fmax(g, 0), 1) * 255;
+	b = fmin(fmax(b, 0), 1) * 255;
+	a = fmin(fmax(a, 0), 1) * 255;
+
+    return ((int)r << 24 |(int) g << 16 |(int) b << 8 |(int) a);
+}
+
 int get_rgba(int r, int g, int b, int a)
 {
+
     return (r << 24 | g << 16 | b << 8 | a);
 }
 
@@ -40,7 +51,45 @@ int map_color(int32_t color1, int32_t color2, double s)
 	return get_rgba(new.r, new.g, new.b, new.a);
 }
 
+float palette_function(float d, float t)
+{
+	return (0.5 + 0.5 * cos(M_PI * 2 * (1.0 * t + d)));
+}
+
+int color_palette(float t)
+{
+	float r = palette_function(-0.721, t);//(0.263, t);
+	float g = palette_function(-0.493, t);//(0.416, t);
+	float b = palette_function(-0.353, t);//(0.557, t);
+
+	return norm_rgba(r, g, b, 1);
+}
+
 void render_pixel(mlx_image_t *img, int i, int j, double num_iter)
+{
+	t_data *data = init_data();
+	int max_iter = data->max_iter;
+	int32_t	color;
+
+	double d = ft_map((double) num_iter, (double[]){0, max_iter}, (double[]){0, 1});
+	static double shift = 0;
+	if(mlx_is_key_down(data->mlx, MLX_KEY_C))
+	{
+		shift += pow(10, -7);
+		if(shift > 1)
+			shift = 0;
+	}
+
+	d = pow(d, 0.4);
+	color = color_palette(d + shift);
+	if (num_iter == max_iter)
+		mlx_put_pixel(img, i, j, BLACK);
+	else
+		mlx_put_pixel(img, i, j, color);
+}
+
+
+void render_pixel_original(mlx_image_t *img, int i, int j, double num_iter)
 {
 	t_data *data = init_data();
 	int max_iter = data->max_iter;
